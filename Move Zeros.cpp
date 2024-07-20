48a748fc3a6a1c0f124d5a211e3a69b94f19e5bc
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-07-20 16:39:40 +0530

    Create Move Zeros.cpp

---
+++ Move Zeros.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+
+        while(count<nums.size())
+        {
+            nums[count]=0;
+            count++;
+        }
+    }
+};
