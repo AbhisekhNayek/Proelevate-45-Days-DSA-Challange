54f4b631fe4d2751775ff3d33619e6ffbb7d49d7
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-09-09 16:50:17 +0530

    Create Rotate Image.cpp

---
+++ Rotate Image.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        reverse(begin(matrix), end(matrix));
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
